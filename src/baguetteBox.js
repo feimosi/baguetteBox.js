9e976b11242021f738f27bd5345718c363f6babc
Alex Drimbe <alex@fivetailors.com>
2016-03-28 15:26:05 +0300

    reorder params in destroy function

--- src/baguetteBox.js
+++ src/baguetteBox.js
@@ -575,9 +575,9 @@
         unbindImageClickListeners();
         unbind(document, 'keydown', keyDownHandler);
         document.getElementsByTagName('body')[0].removeChild(document.getElementById('baguetteBox-overlay'));
-        currentIndex = 0;
-        currentGallery = [];
         data = {};
+        currentGallery = [];
+        currentIndex = 0;
     }
 
     return {
