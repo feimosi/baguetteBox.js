608d0dad3d0e7bce01da9f77c73f1f88c8f1287e
Celso Bessa <celso.bessa@2aces.com.br>
2016-10-01 16:09:51 -0300

    # Adds public showImage method
    - adds a public method allowing to open overlay with arbitrary image from galleries
    - makes data object public

--- src/baguetteBox.js
+++ src/baguetteBox.js
@@ -603,6 +603,15 @@
         }
         return returnValue;
     }
+    
+    // opens specific image
+    function showImage(selector, imageIndex) {
+	    if ( null === imageIndex || undefined === imageIndex || '' === imageIndex ){
+		    imageIndex = 0;
+	    }
+	    var activeImage = baguetteBox.data[selector].galleries[0][imageIndex];
+	    activeImage.eventHandler(activeImage);
+    }
 
     function updateOffset() {
         var offset = -currentIndex * 100 + '%';
@@ -692,9 +701,11 @@
 
     return {
         run: run,
+        data: data,
         destroy: destroyPlugin,
         showNext: showNextImage,
-        showPrevious: showPreviousImage
+        showPrevious: showPreviousImage,
+        showImage: showImage,
     };
 
 }));
