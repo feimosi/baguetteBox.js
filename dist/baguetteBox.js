db8a5418ed4d239b3c78fe1a1147cb54e80df25d
Marek Grzybek <feimosi@onet.pl>
2025-11-10 00:26:40 +0100

    Build update

--- dist/baguetteBox.js
+++ dist/baguetteBox.js
@@ -68,7 +68,7 @@
     // If set to true ignore touch events because animation was already fired
     var touchFlag = false;
     // Regex pattern to match image files
-    var regex = /.+\.(gif|jpe?g|png|webp)/i;
+    var regex = /.+\.(gif|jpe?g|png|webp|avif)/i;
     // Object of all used galleries
     var data = {};
     // Array containing temporary images DOM elements
@@ -686,7 +686,10 @@
     }
 
     function updateOffset() {
-        var offset = -currentIndex * 100 + '%';
+        var isRtl = document.querySelectorAll('html')[0].getAttribute('dir') === 'rtl';
+        var percentage = isRtl ? -100 : 100;
+        var offset = -currentIndex * percentage + '%';
+
         if (options.animation === 'fadeIn') {
             slider.style.opacity = 0;
             setTimeout(function() {
